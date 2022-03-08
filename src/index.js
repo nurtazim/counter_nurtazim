6285eb7343dbccc546a19b07532e1998a08d3f6f
nurtazim <bakytovnurtazim@gmail.com>
2022-03-08 15:49:39 +0600

    counter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const decrement_button=document.getElementById("decrement")
+const increment_button=document.getElementById("increment")
+const countDisplay=document.getElementById("count-display")
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+increment_button.addEventListener("click",()=>{
+   const oldValue=Number(countDisplay.value);
+    countDisplay.value=oldValue+1
+})
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+decrement_button.addEventListener("click",()=>{
+   const oldValue=Number(countDisplay.value);
+    countDisplay.value=oldValue-1
+})
